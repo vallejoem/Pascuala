537d81cc808d92839483bf4ceb4046679967d3d0
vallejoem <emiliano.vallejo@gmail.com>
2024-01-13 10:41:57 -0300

    con carousel de react-responsive-carousel funcionando

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 import { Norican, Borel } from 'next/font/google'
 import { Product } from '../app/types/types';
 import { useState, useEffect } from 'react';
-import { Carousel } from 'flowbite-react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 const norican = Norican({
@@ -24,11 +25,23 @@ export default function Home() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 ">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
           <h2 className={`${borel.className} pt-3 text-3xl font-bold text-fuchsia-950`}>Colecciones Primavera-Verano</h2>
-          <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <Carousel slideInterval={5000}>
-              <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide1.png" alt="..." />
-              <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide2.jpg" alt="..." />
-              <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide3.jpg" alt="..." />
+          <div className="h-full">
+            <Carousel
+              showArrows={true}
+              showThumbs={false}
+              autoPlay={true}
+              interval={5000}
+              infiniteLoop={true}
+            >
+              <div>
+                <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide1.png" alt="..." />
+              </div>
+              <div>
+                <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide2.jpg" alt="..." />
+              </div>
+              <div>
+                <img className='object-cover w-full h-full' src="http://localhost:3500/public/carousel/slide3.jpg" alt="..." />
+              </div>
             </Carousel>
           </div>
 
