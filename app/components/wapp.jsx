339bd333ea58689c7992637efa4956b6c84561e9
vallejoem <emiliano.vallejo@gmail.com>
2024-10-21 13:15:54 -0300

    solucionado el problema de wapp

--- app/components/wapp.jsx
+++ app/components/wapp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Wapp = () => {
